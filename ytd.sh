401cb6238d21ba8298666adabfcd74e0ef2d7fa6
yahgoo <kmsums@gmail.com>
2022-06-09 13:17:54 +0800

    Update ytd.sh

--- ytd.sh
+++ ytd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $1
-yt-dlp $1 yt-dlp --sub-lang en --convert-subs srt --write-auto-sub  -f mp4  --no-check-certificate --verbose 
+yt-dlp $1 --sub-lang en --convert-subs srt --write-auto-sub  -f mp4  --no-check-certificate --verbose 
